072d644b01d8fb3ae168cde979ba107ee2706136
Yashvishnoi <jivishnoi@gmail.com>
2020-08-24 21:28:29 +0530

    Switchcase

---
+++ ladderif.sh
@@ -0,0 +1,8 @@
+number1=5; number2=5
+if [ $number1 -gt $number2 ]; then
+	echo "$number1 is greater than $number2" 
+elif [ $number1 -lt $number2 ] ; then 
+	echo "$number1 is lesser than $number2"
+else 
+	echo "$number1 is equal to $number2"
+fi
